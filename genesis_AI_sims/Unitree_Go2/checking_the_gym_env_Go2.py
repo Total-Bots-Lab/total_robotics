060ffd90084da348e832f0d47f39d6ff0a6fa8ff
ct-nemo13 <initiative.v0@gmail.com>
2025-06-28 00:31:38 +0300

    committ

--- genesis_AI_sims/Unitree_Go2/checking_the_gym_env_Go2.py
+++ genesis_AI_sims/Unitree_Go2/checking_the_gym_env_Go2.py
@@ -12,7 +12,7 @@ import os
 
 
 'Registering and Calling the Gym Environment'
-gym.register(id='Go2_Genesis_Env', entry_point='main_Env_Go2_v0_basic_structure:Go2_Genesis_Env')
+gym.register(id='Go2_Genesis_Env', entry_point='2_go2_env_v0:Go2_Genesis_Env')
 env = gym.make('Go2_Genesis_Env') 
 
 'Number of Episodes'
@@ -25,7 +25,7 @@ for episode in range(episodes):
     'Starting countdown to measure the time taken for one episode'
     start = time.time()
     done = False
-    obs = env.reset()
+    obs, info = env.reset()
     print('\nObservation after Reset:', obs)
     total_reward = 0
     score = 0
